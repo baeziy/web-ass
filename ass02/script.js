a7441ee2668ef2130d784fbd00a80fdeea70c8c8
Mustafa Kamal <mustfakmalik@gmail.com>
2022-12-15 09:03:48 +0500

    ass02 done

--- ass02/script.js
+++ ass02/script.js
@@ -4,5 +4,12 @@ function task1() {
   var z = document.getElementById("p3").value.split(" ");
 
   var commonWords = _.intersection(x, y, z);
-  document.getElementById("P1RES").innerHTML = commonWords;
+  var resultP1 = _.head(_(x).countBy().entries().maxBy(_.last));
+  var resultP2 = _.head(_(y).countBy().entries().maxBy(_.last));
+  var resultP3 = _.head(_(z).countBy().entries().maxBy(_.last));
+
+  document.getElementById("PRES").innerHTML = commonWords;
+  document.getElementById("P1RES").innerHTML = resultP1;
+  document.getElementById("P2RES").innerHTML = resultP2;
+  document.getElementById("P3RES").innerHTML = resultP3;
 }
